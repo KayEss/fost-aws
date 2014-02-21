32a19611064db4c353a358a699307375e9812a39
Kirit Sælensminde <k@kirit.com>
2014-02-21 14:34:23 +0700

    Fixed up code layout.

--- Cpp/include/fost/s3.hpp
+++ Cpp/include/fost/s3.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2014, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -25,20 +25,23 @@ namespace fostlib {
 
 
             void FOST_AWS_DECLSPEC rest_authentication(
-                const string &account, const ascii_string &bucket, http::user_agent::request &request
-            );
+                const string &account, const ascii_string &bucket,
+                http::user_agent::request &request);
+
 
 
             class FOST_AWS_DECLSPEC file_info {
                 boost::shared_ptr< http::user_agent::response > m_response;
                 public:
-                    file_info(const http::user_agent &, const ascii_string &bucket, const boost::filesystem::wpath &);
+                    file_info(const http::user_agent &, const ascii_string &bucket,
+                        const boost::filesystem::wpath &);
 
                     accessors< const boost::filesystem::wpath > path;
 
                     bool exists() const;
                     nullable< string > md5() const;
             };
+
 
             class FOST_AWS_DECLSPEC bucket {
                 http::user_agent m_ua;
@@ -50,7 +53,8 @@ namespace fostlib {
                     accessors< const ascii_string > name;
 
                     file_info stat(const boost::filesystem::wpath &) const;
-                    void put(const boost::filesystem::wpath &file, const boost::filesystem::wpath &location) const;
+                    void put(const boost::filesystem::wpath &file,
+                        const boost::filesystem::wpath &location) const;
             };
 
 
