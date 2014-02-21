7834901986025bd445ce954dd9c429eef9630f09
Kirit Sælensminde <k@kirit.com>
2014-02-21 14:33:17 +0700

    Fixed up the layout.

--- Cpp/fost-s3/s3authen.cpp
+++ Cpp/fost-s3/s3authen.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009-2010, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2014, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -14,11 +14,12 @@ using namespace fostlib;
 using namespace fostlib::aws::s3;
 
 
-namespace {
-}
-
-void fostlib::aws::s3::rest_authentication(const string &account, const ascii_string &bucket, http::user_agent::request &request) {
-    hmac signature(sha1, setting< string >::value(L"S3 account/" + account, L"API secret"));
+void fostlib::aws::s3::rest_authentication(
+    const string &account, const ascii_string &bucket,
+    http::user_agent::request &request
+) {
+    hmac signature(sha1, setting< string >::value(
+        L"S3 account/" + account, L"API secret"));
     signature << request.method() << "\n";
 
     if ( request.headers().exists("Content-MD5") )
@@ -31,10 +32,11 @@ void fostlib::aws::s3::rest_authentication(const string &account, const ascii_st
 
     signature << request.headers()["Date"].value() << "\n";
 
-    signature << "/" << coerce< string >( bucket ) << coerce< string >( request.address().pathspec().underlying() );
+    signature << "/" << coerce< string >( bucket ) <<
+        coerce< string >( request.address().pathspec().underlying() );
 
     request.headers().set("Authorization", L"AWS " +
         setting< string >::value(L"S3 account/" + account, L"API key") + L":" +
-        coerce< string >( coerce< base64_string >( signature.digest() ) )
-    );
+        coerce< string >( coerce< base64_string >( signature.digest() ) ));
 }
+
